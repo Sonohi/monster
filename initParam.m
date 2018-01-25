19f6ba768719538c076268176a1881ef2e993cfb
Matteo <matart@fotonik.dtu.dk>
2018-01-25 16:45:10 +0100

    Added logic to run button and setters

--- initParam.m
+++ initParam.m
@@ -14,6 +14,11 @@ Param.seed = 42;% Integer used for the simulation seed
 % Boolean to save a whole LTE frame for the macro eNodeB for testing
 Param.saveFrame = 1;
 
+%% Draw functions
+Param.generateHeatMap = 0;% Boolean to control the generation of a heatmap of the pathloos in the scenario
+Param.heatMapType = 'perStation';% String to control the type of heatmap
+Param.heatMapRes = 10;% Heatmap resoultion in metresse
+
 %% Network layout
 Param.numSubFramesMacro = 50;% Integer used to set the number of RBs for a macro eNodeB
 Param.numSubFramesMicro = 25;% Integer used to set the number of RBs for a micro eNodeB
@@ -25,12 +30,9 @@ Param.microUniformRadius = 100;% Double radius of distance from centre for micro
 Param.macroHeight = 35;% Double used to specify the height in metres of the macro eNodeBs
 Param.microHeight = 25;% Double used to specify the height in metres of the micro eNodeBs
 Param.ueHeight = 1.5;% Double used to specify the height in metres of the UEs
-Param.buildingHeight = [20,50];% Double interval used to specify the height interval in metres of the buildings
 Param.numUsers = 4;% Integer used for the number of UEs
 Param.mobilityScenario = 'static';% Integer to choose the mobility scenario (pedestrian, vehicular, static, superman)
 Param.buildings = 'mobility/buildings.txt';% Path for loading the file with the buildings
-Param.utilLoThr = 1;% Integer for the threshold for the low utilisation range (>= 1)
-Param.utilHiThr = 100;% Integer for the threshold for the high utilisation range (<= 100)
 Param.trafficModel = 'fullBuffer';% Traffic model
 %% Physical layer
 Param.ulFreq = 1747.5;% Double used for the uplink carrier frequency in MHz
@@ -50,12 +52,8 @@ Param.channel.region = 'DenseUrban';% String to control the channel region
 Param.nboRadius = 100;% Double to set the maximum radius within which eNodeBs are considered as neighbours in metres
 Param.tHyst = 0.002;% Double to set the hysteresis timer threshold in s
 Param.tSwitch = 0.001;% Double to set the eNodeB switching on/off timer in s
-Param.icScheme = 'none';% String to choose the intereference coordination scheme (currently only 'none')
-
-%% Draw functions
-Param.generateHeatMap = 0;% Boolean to control the generation of a heatmap of the pathloos in the scenario
-Param.heatMapType = 'perStation';% String to control the type of heatmap
-Param.heatMapRes = 10;% Heatmap resoultion in metres
+Param.utilLoThr = 1;% Integer for the threshold for the low utilisation range (>= 1)
+Param.utilHiThr = 100;% Integer for the threshold for the high utilisation range (<= 100)
 
 %% Scheduling
 Param.scheduling = 'roundRobin';% String for the scheduling policy to use (currently only 'roundRobin')
@@ -63,7 +61,6 @@ Param.refreshAssociationTimer = 0.001;% Double to choose the interval in s to ru
 Param.PRACHInterval = 10; %Given as the number of subframes between each PRACH.
 Param.icScheme = 'none';
 Param.absMask = [1,0,1,0,0,0,0,0,0,0];
-
 
 %% HARQ
 Param.harq.rtxMax = 3;% Integer to choose the maximum number of HARQ retransmissions
@@ -76,9 +73,9 @@ Param.arq.rtxMax = 1;% Integer to choose the maximum number of ARQ retransmissio
 Param.pucchFormat = 2;% PUCCH format (only 2 and 3 work)
 Param.handoverTimer = 0.01;% X2 Handover timer in s (time needed from starting and handover to its completion)
 
-
 %%%%% SETUP STUFF - DON'T TOUCH UNLESS YOU KNOW WHAT YOU'RE DOING
 Param.buildings = load(Param.buildings);
+Param.buildingHeight = [20,50];% Double interval used to specify the height interval in metres of the buildings
 Param.area = [min(Param.buildings(:, 1)), min(Param.buildings(:, 2)), ...
 	max(Param.buildings(:, 3)), max(Param.buildings(:, 4))];
 Param.buildings(:,5) = randi([Param.buildingHeight],[1 length(Param.buildings(:,1))]);
