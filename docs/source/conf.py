600b9083af341f728d5811647a5fc6e7c6ebca59
Jakob Thrane <jathr@fotonik.dtu.dk>
2018-04-20 10:54:52 +0200

    Added logos to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,7 +86,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -102,6 +104,7 @@ html_static_path = ['_static']
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
+html_logo = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../resources/graphics/logo_small.svg'))
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
