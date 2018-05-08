28015c40a4a4cf12be990552d4ea2156286d57fd
Matteo <matart@fotonik.dtu.dk>
2018-05-08 14:48:30 +0200

    Updated batch system for log limitations

--- batch_main.m
+++ batch_main.m
@@ -1,9 +1,15 @@
-for iUtil = 1:4
-    for k = 1:3
-			try
-				batch_util_users_sim(iUtil, k);
-			catch ME
-				sonohilog(sprintf('Error in simulate for util index %i and users index %i',iUtil, k),'WRN');
-			end			
-    end
+%Set Log level
+setpref('sonohiLog','logLevel',4);
+dateStr = datestr(datetime, 'yyyy-mm-dd_HH.MM.SS');
+logName = strcat('logs/', dateStr,'.txt'); 
+setpref('sonohiLog', 'logToFile', 1);
+setpref('sonohiLog', 'logFile', logName);
+
+parfor i = 1:4
+	try
+		batch_users_sim(i);
+	catch ME
+		sonohilog(sprintf('Error in simulate for users index %i', i),'WRN');
+		sonohilog(ME.stack);
+	end			
 end
