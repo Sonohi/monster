dd41b8576e6a9d9da4d33fd93800ee5a49262d86
Jakob Thrane <jathr@fotonik.dtu.dk>
2018-09-13 14:38:32 +0200

    Fix of 3GPP3901 example

--- ch/examples/Example3GPP38901.m
+++ ch/examples/Example3GPP38901.m
@@ -1,5 +1,6 @@
 clear all
 close all
+initParam
 load('SimulationParameters.mat');
 Param.numMacro = 1;
 Param.numMicro = 0;
@@ -12,6 +13,7 @@ Param.channel.enableShadowing = true;
 Param.channel.LOSMethod = '3GPP38901-probability';
 Param.channel.modeDL = '3GPP38901';
 Param.area = [-1000, -1000, 1000, 1000];
+Param.channel.region = struct();
 Param.channel.region.macroScenario = 'UMa';
 
 
@@ -31,7 +33,7 @@ ChannelUMa = ChBulk_v2(Station, User, Param);
 Param.channel.region.macroScenario = 'RMa';
 ChannelRMa = ChBulk_v2(Station, User, Param);
 
-
+whwat :j
 Station.Users = struct('UeId', User.NCellID, 'CQI', -1, 'RSSI', -1);
 Station.ScheduleDL(1,1).UeId = User.NCellID;
 User.ENodeBID = Station.NCellID;
@@ -39,7 +41,7 @@ User.ENodeBID = Station.NCellID;
 % A full LTE frame is stored in Tx.Frame which can be used to debug and
 % test.
 Station.Tx.Waveform = Station.Tx.Frame;
-Station.Tx.WaveformInfo =Station.Tx.FrameInfo;
+Station.Tx.WaveformInfo = Station.Tx.FrameInfo;
 Station.Tx.ReGrid = Station.Tx.FrameGrid;
 
 % Traverse channel
@@ -63,7 +65,7 @@ User.Rx = User.Rx.referenceMeasurements(Station);
 setpref('sonohiLog','logLevel', 4);
 Station.Position(1:2) = [0, 0];
 % Set up coordinates of UE
-sweepRes = 10; %1m
+sweepRes = 20; %1m
 
 % Get area size
 lengthXY = [Param.area(1):sweepRes:Param.area(3); Param.area(2):sweepRes:Param.area(4)];
@@ -84,13 +86,6 @@ for Xpos = 1:length(lengthXY(1,:))
         catch ME
             
         end
-        % Get offset
-        %ue.Rx.Offset = lteDLFrameOffset(Station, ue.Rx.Waveform);
-
-        % Apply offset
-        %ue.Rx.Waveform = ue.Rx.Waveform(1+ue.Rx.Offset:end);
-        % UE reference measurements
-        %ue.Rx = ue.Rx.referenceMeasurements(Station);
         
         resultsUMa{Xpos,Ypos} = ueUMa.Rx.ChannelConditions;
         resultsRMa{Xpos,Ypos} = ueRMa.Rx.ChannelConditions;
@@ -99,41 +94,80 @@ for Xpos = 1:length(lengthXY(1,:))
     end
 end
 
-%%
+%% Create visualization vectors/matrices
 
-resultsLOS = nan(N,N);
-resultsPL = nan(N,N);
-resultsLSP = nan(N,N);
-resultsLOSprop = nan(N,N);
+UMaResultsLOS = nan(N,N);
+UMaResultsPL = nan(N,N);
+UMaResultsLSP = nan(N,N);
+UMaResultsLOSprop = nan(N,N);
+
+RMaResultsLOS = nan(N,N);
+RMaResultsPL = nan(N,N);
+RMaResultsLSP = nan(N,N);
+RMaResultsLOSprop = nan(N,N);
 for Xpos = 1:length(lengthXY(1,:))
     
     for Ypos = 1:length(lengthXY(2,:))
         
-        resultsLOS(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LOS;
-        resultsPL(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.pathloss;
-        resultsLSP(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LSP;
-        resultsLOSprop(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LOSprop;
+        UMaResultsLOS(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LOS;
+        UMaResultsPL(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.pathloss;
+        UMaResultsLSP(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LSP;
+        UMaResultsLOSprop(Xpos,Ypos) = resultsUMa{Xpos,Ypos}.LOSprop;
+        
+        RMaResultsLOS(Xpos,Ypos) = resultsRMa{Xpos,Ypos}.LOS;
+        RMaResultsPL(Xpos,Ypos) = resultsRMa{Xpos,Ypos}.pathloss;
+        RMaResultsLSP(Xpos,Ypos) = resultsRMa{Xpos,Ypos}.LSP;
+        RMaResultsLOSprop(Xpos,Ypos) = resultsRMa{Xpos,Ypos}.LOSprop;
         
     end
 end
 
+%% Plotting
+
 close all
 
 figure
-contourf(lengthXY(1,:), lengthXY(2,:), resultsPL)
-colorbar
+contourf(lengthXY(1,:), lengthXY(2,:), UMaResultsPL)
+caxis([70 150])
+c = colorbar;
+c.Label.String = 'loss [dB]';
+c.Label.FontSize = 12;
 colormap jet
+title('UMa \mu pathloss, 1.84 GHz')
+xlabel('X [m]')
+ylabel('Y [m]')
+
+
 
 figure
-contourf(lengthXY(1,:), lengthXY(2,:), resultsLOS,1)
+contourf(lengthXY(1,:), lengthXY(2,:), RMaResultsPL)
+caxis([70 150])
+c = colorbar;
+c.Label.String = 'loss [dB]';
+c.Label.FontSize = 12;
+colormap jet
+title('RMa \mu pathloss, 1.84 GHz')
+xlabel('X [m]')
+ylabel('Y [m]')
+
+
+
+
+figure
+contourf(lengthXY(1,:), lengthXY(2,:), UMaResultsLOS,1)
+title('LOS state for UMa')
+
+figure
+contourf(lengthXY(1,:), lengthXY(2,:), RMaResultsLOS,1)
+title('LOS state for RMa')
 colormap summer
 
 figure
-contourf(lengthXY(1,:), lengthXY(2,:), resultsLSP)
+contourf(lengthXY(1,:), lengthXY(2,:), UMaResultsLSP)
 colorbar
 colormap jet
 
 figure
-contourf(lengthXY(1,:), lengthXY(2,:), resultsLOSprop)
+contourf(lengthXY(1,:), lengthXY(2,:), UMaResultsLOSprop)
 colorbar
 colormap jet
