2218d25bd30383e1327b91d3778dec6d0fd35a2c
Jakob Thrane <jathr@fotonik.dtu.dk>
2019-01-28 13:43:55 +0100

    Removed unused code from refresh user association.

--- phy/refreshUsersAssociation.m
+++ phy/refreshUsersAssociation.m
@@ -9,23 +9,12 @@ function [Users, Stations] = refreshUsersAssociation(Users, Stations, Channel, C
 	% :Users: Array<UserEquipment> instances with associated eNodeBs
 	% :Stations: Array<EvolvedNodeB> instances with associated UEs
 	%
-
-	% Create a local copy
-	StationsC = copy(Stations);
-	%Set the stored dummy frame as current waveform
-	for iStation = 1:length(Stations)
-		StationsC(iStation).Tx.Waveform = StationsC(iStation).Tx.Frame;
-		StationsC(iStation).Tx.WaveformInfo = StationsC(iStation).Tx.FrameInfo;
-		StationsC(iStation).Tx.ReGrid = StationsC(iStation).Tx.FrameGrid;
-		StationsC(iStation).Users(1:Config.Ue.number) = struct('UeId', -1, 'CQI', -1, 'RSSI', -1);
-	end
 	
 	% Now loop the users to get the association based on the signal attenuation
 	for iUser = 1:length(Users)
-		
 			
 		% Get the ID of the eNodeB this UE has the best signal to 
-		targetEnbID = Channel.getENB(Users(iUser), StationsC, 'downlink');
+		targetEnbID = Channel.getENB(Users(iUser), Stations, 'downlink');
 
 		% Check if this UE is initialised already to a valid eNodeB. If not, don't perform HO, but simply associate
 		if Users(iUser).ENodeBID == -1
