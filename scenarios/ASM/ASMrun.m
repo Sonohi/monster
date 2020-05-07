d9381d13c0176234426ab706b739451ee405bd83
Abdullah <abdullah.saei@tuhh.de>
2020-05-07 16:03:00 +0200

    Adding Parser file

--- scenarios/ASM/ASMrun.m
+++ scenarios/ASM/ASMrun.m
@@ -5,24 +5,26 @@ close all
 Config = MonsterConfig(); % Get template config parameters
 
 Config.Logs.logToFile = 2; % 0 only console | 1 only file | 2 both
-SimulationPlot.runtimePlot = 1; 
+Config.SimulationPlot.runtimePlot = 1; 
+Config.Runtime.simulationRounds = 5; % each round ITT (subframe 1 ms)
 
 %ASM paper duplication
 Config.Scenario = 'ASM_reproduction';
-Config.MacroEnb.sitesNumber = 19;
+Config.MacroEnb.sitesNumber = 8;
 Config.MacroEnb.cellsPerSite = 3;
 Config.MacroEnb.numPRBs = 100; %50 corresponds to a bandwidth of 10MHz
 Config.MacroEnb.height = 30;
 Config.MacroEnb.positioning = 'centre';
-Config.MacroEnb.ISD = 500; %intersite distance in meters
-Config.MacroEnb.Pmax = 10^(46/10)/1e3; %46dBm converted to W
+Config.MacroEnb.ISD = 300; %intersite distance in meters
+%Config.MacroEnb.Pmax = 10^(46/10)/1e3; %46dBm converted to W ~ 40W
 
 %Thermal noise = -174dBm/Hz
+%{
 Config.MacroEnb.noiseFigure = 5; %dB
 Config.Ue.noiseFigure = 7; %dB
 Config.MacroEnb.antennaGain = 8; % dBi
 Config.Ue.antennaGain = 0; %dBi
-
+%}
 %Ue Transmit power in dBm = 23. 
 %Percentage of high loss and low loss: 20/80 (high/low)
 
@@ -35,7 +37,7 @@ Config.MicroEnb.sitesNumber = 0;
 %Mobility modelling: Fixed and idential speed v of all UEs, random direction
 %UE speed: indoor: 3km/h    outdoor: 30km/h (in car)
 Config.Mobility.scenario = 'pedestrian';
-Config.Mobility.Velocity = 0.01;
+Config.Mobility.Velocity = 0;
 
 Config.Traffic.primary = 'fullBuffer';
 Config.Traffic.mix = 0; %0-> no mix, only primary
@@ -43,8 +45,7 @@ Config.Traffic.arrivalDistribution = 'Poisson'; % Static | Uniform | Poisson
 Config.Traffic.poissonLambda = 9;
 
 %Simulation bandwidth: 20 MHz for TDD, 10 MHz+10 MHz for FDD
-Config.Ue.number = 30; %TODO: 14 different from 1 to 60
-Config.Ue.height = 1.5; %meters
+Config.Ue.number = 19; %TODO: 14 different from 1 to 60
 
 Logger = MonsterLog(Config);
 Logger.log('(MAIN) configured simulations and started initialisation', 'NFO');
@@ -55,7 +56,7 @@ Logger.log('(MAIN) creating main simulation instance', 'NFO');
 Simulation = Monster(Config, Logger);
 Simulation.Logger.log('(MAIN) main simulation instance created', 'NFO');
 
-for iRound = 0:(Config.Runtime.simulationRounds - 1)
+for iRound = 0:(Simulation.Runtime.totalRounds - 1)
     Simulation.setupRound(iRound);
     
 	Simulation.Logger.log(sprintf('(MAIN) simulation round %i, time elapsed %f s, time left %f s',...
